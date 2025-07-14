4dc1592f3339cbe95a6a6103cc2fe1fcd2eed496
Rehan Gauri <rehangauri310@gmail.com>
2025-07-14 14:45:20 +0530

    update mongoose.connect depreciated 2 line code

--- app.js
+++ app.js
@@ -36,10 +36,7 @@ app.use('/api/suggestions', suggestionRoutes);
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
 .then(() => {
   console.log("✅ MongoDB Connected");
 
